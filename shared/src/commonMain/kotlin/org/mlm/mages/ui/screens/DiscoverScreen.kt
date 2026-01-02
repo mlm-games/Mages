89f9ffd9cbbb970bcc91c8ef3aa939f68d8099a9
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-01-02 17:34:04 +0530

    Change discover screen's navBack icon (fix #15)

--- shared/src/commonMain/kotlin/org/mlm/mages/ui/screens/DiscoverScreen.kt
+++ shared/src/commonMain/kotlin/org/mlm/mages/ui/screens/DiscoverScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Group
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
@@ -51,7 +52,7 @@ fun DiscoverScreen(
                 title = { Text("Discover") },
                 navigationIcon = {
                     IconButton(onClick = onClose) {
-                        Icon(Icons.Default.Group, contentDescription = null)
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
                     }
                 }
             )
