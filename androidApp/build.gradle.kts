919475e755dab5242be8cbd95741c747988ce5c1
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-02-22 13:34:10 +0530

    chore: bump ver

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 731
-        versionName = "3.0.2"
+        versionName = "3.1.0"
 
         // have to keep versionName here for fdroid, do not change
 
