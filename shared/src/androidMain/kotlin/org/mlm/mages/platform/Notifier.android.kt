a0b09bfd1b9fcbb469236e104e8794467747e143
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-04-09 04:48:57 +0530

    Android: fix Bubbles getting cancelled on opening a notification (fix #76, #73)

--- shared/src/androidMain/kotlin/org/mlm/mages/platform/Notifier.android.kt
+++ shared/src/androidMain/kotlin/org/mlm/mages/platform/Notifier.android.kt
@@ -21,7 +21,6 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import org.koin.core.context.GlobalContext
 import org.mlm.mages.MatrixService
-import org.mlm.mages.push.AndroidNotificationHelper
 import org.mlm.mages.push.PREF_INSTANCE
 import org.mlm.mages.push.PusherReconciler
 import org.mlm.mages.settings.AppSettings
@@ -71,7 +70,6 @@ actual object Notifier {
         currentRoomId = roomId
         if (roomId != null) {
             val ctx = appContextOrNull() ?: return
-            AndroidNotificationHelper.cancelRoomNotification(ctx, roomId)
             runBlocking {
                 val settingsRepo = SettingsProvider.get(ctx)
                 val settings = settingsRepo.flow.first()
