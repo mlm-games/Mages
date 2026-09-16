955bd34b85f211fb9a710bc9cd81a3bf80bd8b56
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-09-16 21:22:15 +0530

    chore: mis

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -28,7 +28,6 @@ kotlin {
 
     android {
         namespace = "org.mlm.mages.shared"
-        ndkVersion = libs.versions.android.ndkVersion.get()
         compileSdk {
             version = release(libs.versions.android.compileSdk.get().toInt()) {
                 minorApiLevel = libs.versions.android.compileSdkMinor.get().toInt()
